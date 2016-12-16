43c6f8924c00d8049555d14880393b289d6baf5c
ziaur rahman <m.ziaur.rahman@gmail.com>
2016-12-16 10:22:11 +0600

    ii

--- js/app.js
+++ js/app.js
@@ -6,12 +6,22 @@ var resultDiv;
 
 document.addEventListener("deviceready", init, false);
 function init() {
+
 	document.querySelector("#startScan").addEventListener("touchend", startScan, false);
 	resultDiv = document.querySelector("#results");
+	
+
 }
 
 function startScan() {
 
+/*	cordova.plugins.diagnostic.isCameraPresent(function(present){
+    console.log("Camera is " + (present ? "present" : "absent"));
+	//alert(""Camera is " + (present ? "present" : "absent")");
+}, function(error){
+    console.error("The following error occurred: "+error);
+	//alert("The following error occurred: "+error);
+});*/
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
 			var s = "Result: " + result.text + "<br/>" +
